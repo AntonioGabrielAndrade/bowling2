4a00ff6aff1f93b555d5421dd290e6cb9b45deb0
Antonio Gabriel Pereira de Andrade <gabriel.ecomp@gmail.com>
2018-11-16 20:51:59 -0200

    Add test for spare game score

--- src/bowling_kata/core.clj
+++ src/bowling_kata/core.clj
@@ -1,5 +1,25 @@
 (ns bowling-kata.core
   (:gen-class))
 
+(defn to-frames [rolls]
+  (partition 2 rolls))
+
+(defn spare? [frame]
+  (and (= 2 (count frame))
+       (= 10 (reduce + frame))))
+
+(defn put-frame-score [frame next-frame scores]
+  (if (spare? frame)
+    (conj scores (+ (reduce + frame) (first next-frame)))
+    (conj scores (reduce + frame))))
+
+(defn frame-scores [frames]
+  (loop [remaining frames
+         scores []]
+    (if (empty? remaining)
+      scores
+      (recur (rest remaining)
+             (put-frame-score (first remaining) (second remaining) scores)))))
+
 (defn score [rolls]
-  (reduce + rolls))
+  (reduce + (frame-scores (to-frames rolls))))
--- test/bowling_kata/core_test.clj
+++ test/bowling_kata/core_test.clj
@@ -2,11 +2,39 @@
   (:require [clojure.test :refer :all]
             [bowling-kata.core :refer :all]))
 
+(deftest to-frames-test
+  (testing "can partition rolls into frames"
+    (is (= [[1 9] [1 2] [1 1] [1 1] [1 1] [1 1] [1 1] [1 1] [1 1] [1 1]]
+           (to-frames [1 9 1 2 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1])))))
+
+(deftest spare?-test
+  (testing "can tell when frame is a spare"
+    (is (spare? [5 5]))
+    (is (spare? [9 1]))
+    (is (not (spare? [2 3])))
+    (is (not (spare? [10])))))
+
+(deftest put-frame-score-test
+  (testing "can compute a simple frame score"
+    (is (= [7] (put-frame-score [5 2] [1 2] []))))
+  (testing "can compute a spare frame score"
+    (is (= [11] (put-frame-score [5 5] [1 2] [])))))
+
+(deftest frames-score-test
+  (testing "can calc simple frame score"
+    (is (= [9 3 2 2 2 2 2 2 2 2]
+          (frame-scores [[1 8] [1 2] [1 1] [1 1] [1 1] [1 1] [1 1] [1 1] [1 1] [1 1]]))))
+  (testing "can calc a spare frame score"
+    (is (= [11 3 2 2 2 2 2 2 2 2]
+           (frame-scores [[1 9] [1 2] [1 1] [1 1] [1 1] [1 1] [1 1] [1 1] [1 1] [1 1]])))))
+
 (deftest score-test
   (testing "can calc zero-game score"
     (is (= 0 (score [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]))))
   (testing "can calc a simple game score"
     (is (= 20 (score [1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1])))
-    (is (= 90 (score [9 0 9 0 9 0 9 0 9 0 9 0 9 0 9 0 9 0 9 0])))))
+    (is (= 90 (score [9 0 9 0 9 0 9 0 9 0 9 0 9 0 9 0 9 0 9 0]))))
+  (testing "can calc a spare-game score"
+    (is (= 29 (score [1 9 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1])))))
 
 (run-tests 'bowling-kata.core-test)

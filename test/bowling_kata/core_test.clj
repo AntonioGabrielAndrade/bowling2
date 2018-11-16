9da9fa2ae3906d8dfc32a268ceff356b2063d15a
Antonio Gabriel Pereira de Andrade <gabriel.ecomp@gmail.com>
2018-11-16 20:25:32 -0200

    Add test for simple game score

--- test/bowling_kata/core_test.clj
+++ test/bowling_kata/core_test.clj
@@ -4,6 +4,9 @@
 
 (deftest score-test
   (testing "can calc zero-game score"
-    (is (= 0 (score [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0])))))
+    (is (= 0 (score [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]))))
+  (testing "can calc a simple game score"
+    (is (= 20 (score [1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1])))
+    (is (= 90 (score [9 0 9 0 9 0 9 0 9 0 9 0 9 0 9 0 9 0 9 0])))))
 
 (run-tests 'bowling-kata.core-test)
